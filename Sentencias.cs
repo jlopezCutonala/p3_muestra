7a7503444d340c1603e8243d94961d7376e2e1b0
Jonathan López <jlopez@fipasoft.com.mx>
2015-02-04 10:27:53 -0600

    Método para la estructura IF

--- Sentencias.cs
+++ Sentencias.cs
@@ -4,4 +4,16 @@ namespace sentencias
     public class Sentencias
     {
     }
+
+    public void ejemploIF(){
+        bool bandera = true;
+        if ( bandera == true )
+        {
+            Console.WriteLine( "La bandera es verdadera." );
+        }
+        else
+        {
+            Console.WriteLine( "La bandera es falsa." );
+        }
+    }
 }
